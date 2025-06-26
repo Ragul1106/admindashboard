c43b4ca056ecb47c7247159c68846a88169c03a8
Ragul116 <ragulranjith1106@gmail.com>
2025-06-26 07:52:30 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
-import Dashboard from './components/Dashboard';
+import Dashboard from './pages/Dashboard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
